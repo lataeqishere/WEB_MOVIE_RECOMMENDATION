cee0901031b64ff65b09bf398accd53aff24605e
DatboisouL <9potae.work@gmail.com>
2024-01-31 20:36:43 +0700

    add routes for catalog page

--- application/routes.py
+++ application/routes.py
@@ -4,3 +4,7 @@ from flask import render_template, url_for
 @app.route("/")
 def index():
     return render_template("index.html", navindex=True)
+
+@app.route("/catalog")
+def catalog():
+    return render_template("catalog.html", navcatalog=True)
