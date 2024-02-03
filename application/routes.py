11deff300d4ef8ae1f4530f13429944da30ec123
DatboisouL <9potae.work@gmail.com>
2024-02-03 11:52:52 +0700

    add if statement in login function

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from application import app
 from flask import Flask, render_template, request, flash, redirect, url_for,session
-
+import sqlite3
 ratings_ = [{'Rating': 1.0,'Name': "test1"},
             {'Rating': 2.0,'Name': "test2"},
             {'Rating': 3.0,'Name': "test3"}]
@@ -8,6 +8,12 @@ ratings_ = [{'Rating': 1.0,'Name': "test1"},
 preds_ = [{'Predicted_Rating': 3.0,'Name': "test1"},
             {'Predicted_Rating': 3.0,'Name': "test2"},
             {'Predicted_Rating': 3.0,'Name': "test3"}]
+
+app.secret_key='eezahahabuak'
+
+con=sqlite3.connect('database.db')
+con.execute("create table if not exists customer(pid integer primary key,name text,email text)")
+con.close()
 
 @app.route("/")
 def index():
@@ -29,9 +35,25 @@ def reviews():
 def about():
     return render_template("about.html", navabout=True)
 
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
-    return render_template("login.html", navlogin=True)
+    if request.method=='POST':
+        name=request.form['name']
+        password=request.form['password']
+        con=sqlite3.connect('database.db')
+        con.row_factory=sqlite3.Row
+        cur=con.cursor()
+        cur.execute('select * from customer where name=? and email=?',(name,password))
+        data=cur.fetchone()
+
+        if data:
+            session['name']=data['name']
+            session['email']=data['email']
+            return redirect('welcome')
+        else:
+            flash('Username and Password Mismatch','danger')
+    
+    return render_template("login.html")
 
 @app.route("/register",methods=['GET','POST'])
 def register():
